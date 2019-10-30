2afae0db7e0784033c2dcc9103ae61a1d3639bb3
Adrien <adrienroux94@gmail.com>
2019-10-30 19:34:10 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/sCrapper.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/ESGI/Annee_3/sCrapper/conf_funcs.c" "D:/ESGI/Annee_3/sCrapper/cmake-build-debug/CMakeFiles/sCrapper.dir/conf_funcs.c.obj"
+  "D:/ESGI/Annee_3/sCrapper/main.c" "D:/ESGI/Annee_3/sCrapper/cmake-build-debug/CMakeFiles/sCrapper.dir/main.c.obj"
+  "D:/ESGI/Annee_3/sCrapper/scrap_funcs.c" "D:/ESGI/Annee_3/sCrapper/cmake-build-debug/CMakeFiles/sCrapper.dir/scrap_funcs.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sCrapper.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sCrapper.dir/conf_funcs.c.obj"
+  "CMakeFiles/sCrapper.dir/main.c.obj"
+  "CMakeFiles/sCrapper.dir/scrap_funcs.c.obj"
+  "libsCrapper.dll.a"
+  "sCrapper.exe"
+  "sCrapper.exe.manifest"
+  "sCrapper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sCrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
